b9d76317cc59a06d1b8270fc28b8c2b751c8113f
André Luis Francisco Junior <33641043+Andre-LFJ@users.noreply.github.com>
2022-02-21 21:02:11 -0300

    agora funciona?

--- Final/Main.py
+++ Final/Main.py
@@ -1,61 +1,127 @@
-import numpy
-import math
+import Entities
+import Logic
+import Render
+from random import randint
+from collections import deque
+
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from OpenGL.arrays import vbo
-width, height = 1000, 768
 
+width, height = 1000, 800
 
-def refresh2d():
-    glViewport(0, 0, width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(0.0, width, 0.0, height, 0.0, 1.0)
-    glMatrixMode (GL_MODELVIEW)
-    glLoadIdentity()
+blu_team = [] # listas de ids de entidades nas equipes
+red_team = []
+blu_points = 0
+red_points = 0
 
-def draw_circle(cx, cy):
-    glBegin(GL_LINE_LOOP)
+def gameLoop():
+    turn = 0
 
-    raio = 5
-    segmentos = 30
-
-    for i in range(segmentos):
-        angulo = (2*numpy.pi*i)/segmentos
+    while(True):
+        Logic.takeTurn(blu_team)
+        Logic.takeTurn(red_team)
         
-        x = raio*math.cos(angulo)
-        y = raio*math.sin(angulo)
+        for id, path in Logic.move_buffer.items(): # para cada movimento no buffer de movimentos
+            # Render.render(game_map) # renderiza o mapa
+            # recupera o movimento e a entidade que fará ele
+            # input("Passou do loop move_buffer")
+            entity = Logic.entity_list[id]
+            while entity.action_points > 0 and len(path) > 0:
+                # input("Passou do loop action_points")
+                entity.action_points -= 1
+                # se a entidade tiver pontos de ação suficientes
+                old_pos = entity.position
+                new_pos = path.popleft()
+                entity.updatePosition(new_pos)                
 
-        glVertex2f(x+cx, y+cy)
+                Logic.occupied_spaces.remove(old_pos)
+                Logic.occupied_spaces.append(new_pos)
 
-    glEnd() 
+                Logic.game_map[old_pos[0]][old_pos[1]] = None
+                Logic.game_map[new_pos[0]][new_pos[1]] = id
 
-def draw():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
-    refresh2d()
-    glColor3f(0.0, 0.0, 1.0)
+                # Render.render(game_map) # renderiza o mapa
+                
+                Render.draw(red_team, blu_team)
 
-    draw_circle(0.5,0.5)
+        for proj, path in Logic.projectile_buffer:
+            current_pos = proj.position
+            next_pos = path.popleft() # ultima posição do projétil
 
-    glutSwapBuffers()
+            # Render.render_proj(proj, next_pos)
+
+            if current_pos in Logic.occupied_spaces:
+                Logic.projectileCollision(proj, current_pos)
+                # Render.render(game_map) # renderiza o mapa
+                del proj
+
+            proj.position = new_pos
+            proj.ttl -= randint(1,5)
+            if proj.ttl <= 0:
+                proj.zeroTTL()
+
+        turn += 1
+        # se uma das equipes perdeu todas as entidades, jogo termina
+        if len(blu_team) == 0:
+            return (0, blu_points)
+        if len(red_team) == 0:
+            return (1, red_points)
+
+        for _, entity in Logic.entity_list.items():
+            entity.resetActionPoints()
+        
+    #Render.draw()
+    #input()
+
+def gameStart():
+
+    # equipe azul começa na parte de baixo do mapa, vermelha no topo do mapa
+    lower_limit_x, upper_limit_x, lower_limit_y, upper_limit_y = 0, Logic.map_size-1, 0, 10
+    Logic.populateTeams(blu_team, lower_limit_x, upper_limit_x, lower_limit_y, upper_limit_y)
+
+    lower_limit_y, upper_limit_y = Logic.map_size-10, Logic.map_size-1
+    Logic.populateTeams(red_team, lower_limit_x, upper_limit_x, lower_limit_y, upper_limit_y)
+
+    lower_limit_x, upper_limit_x, lower_limit_y, upper_limit_y = 10, Logic.map_size-10, 30, 60
+    total_elements = randint(20,30)
+    for _ in range(total_elements):
+        Entities.generateRandomTerrain(lower_limit_x, upper_limit_x, lower_limit_y, upper_limit_y)
+    
+    Logic.loadMap()
+
+def getBluTeam():
+    return blu_team
+def getRedTeam():
+    return red_team
 
 def main():
+    gameStart()
     glutInit()
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
     glutInitWindowSize(width, height)
     glutInitWindowPosition(0, 0)
     glutCreateWindow("Trabalho Final de CGR")
-    glutDisplayFunc(draw)
-    glutIdleFunc(draw)
-    glutMainLoop()
+    glutDisplayFunc(Render.draw)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(40, 1, 1, 40)
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(0, 0, 10,
+              0, 0, 0,
+              0, 1, 0)
+    glPushMatrix()
+    glutIdleFunc(Render.draw)
+    #glutMainLoop()
+
+    #print(getBluTeam())
+    #print(getRedTeam())
+    #print("socorro")
+    result = gameLoop()
+    if result[0] == 0: # Renderizar esse texto na tela
+        print("Red Wins !")
+    else:
+        print("Blu Wins !")
 
 if __name__ == "__main__":
     main()
-    
-# https://stackoverflow.com/questions/22444450/drawing-circle-with-opengl
-# https://noobtuts.com/python/opengl-introduction
-# https://cyrille.rossant.net/2d-graphics-rendering-tutorial-with-pyopengl/
-# http://pyopengl.sourceforge.net/documentation/index.html
-# http://pyopengl.sourceforge.net/context/tutorials/index.html
